44e0cd0f2ce710f0c6e7d77911b18ab225e6357c
carlosstivengrisales <carlosstivengrisales@gmail.com>
2022-08-10 11:23:10 -0500

    se realiza borran carpetas iniciales y se crea doctor

---
+++ doctor.java
@@ -0,0 +1,11 @@
+class doctor {
+    int id;
+    String name;
+    String speciality;
+
+    public void showname()
+    {
+        System.out.print(name + " " + speciality)  ;
+    }
+
+}
--- main.java
+++ main.java
@@ -1,18 +1,15 @@
+import java.util.jar.Attributes.Name;
+
 class main {
     public static void main(String[] args) {
-        System.out.println("hola mundo");
 
-        Car car = new Car("ABD456", new Account("carlos Grisales", "and123"));
-        car.passegenger = 4;
-        car.printCar();
+        doctor mydoctor = new doctor();
+        mydoctor.name = "carlos grisales";
+        mydoctor.speciality = "Cardiologo";
 
-        Car car2 = new Car("ABD455", new Account("Juan Villa", "ADF5455"));
-        car2.passegenger = 3;
-        car2.printCar();
-
-        Car car3 = new Car("BAD546", new Account("Junior Rojas", "AAA123"));
-        car3.passegenger = 5;
-        car3.printCar();
+      mydoctor.showname();
+    
 
     }
+
 }
