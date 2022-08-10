9a08c67d5622e83d2f76379ecfd361991f3c51a6
carlosstivengrisales <carlosstivengrisales@gmail.com>
2022-08-10 15:50:10 -0500

    Se agran meses de citas

--- main.java
+++ main.java
@@ -3,23 +3,19 @@ import java.util.jar.Attributes.Name;
 class main {
     public static void main(String[] args) {
 
-        doctor mydoctor = new doctor();
-        mydoctor.name = "carlos grisales";
-        mydoctor.speciality = "Cardiologo";
 
-
-      mydoctor.showname();
 
       uimenu.showMenu();
 
 
       
-
      
 
    
-    
+
 
     }
+
+   
 
 }
--- uimenu.java
+++ uimenu.java
@@ -2,36 +2,41 @@ import java.util.Scanner;
 
 public class uimenu {
 
+    public static String[] MONTH = {"Enero", "Febrero", "Marzo" , "Abril", "Mayo", "Junio" , "Julio", "Agosto", "Septiembre" , "Octubre", "Noviembre", "Diciembre"};
+
     static void showMenu() {
-        System.out.print("Bienvenido al menu ");
-        System.out.print("Seleccionona la opcion deseada ");
+        System.out.print("Bienvenido al menu \n");
+        System.out.print("Seleccionona la opcion desead○ \n ");
 
         int response = 10;
         do {
-            System.out.print("1. Doctor ");
-            System.out.print("2. Paciente ");
-            System.out.print("0. salir ");
+            System.out.print("MENU PRINCIPAL \n ");
+            System.out.print("1. Doctor \n ");
+            System.out.print("2. Paciente  \n");
+            System.out.print("0. salir \n ");
 
             Scanner sc = new Scanner(System.in);
             response = Integer.valueOf(sc.nextLine());
 
             switch (response) {
                 case 1:
-                    System.out.print("Doctor ");
+                    System.out.print("Doctor \n");
                     break;
 
                 case 2:
-                    System.out.print("Paciente ");
+                    System.out.print("Paciente \n");
+                    response = 0;
+                    showPatienMenu();
                     break;
 
                 case 0:
-                    System.out.print("saliste del sistema ");
+                    System.out.print("saliste del sistema \n");
                     break;
 
                 default:
-                    System.out.print("Debes seleccionar una opcion correcta ");
-            }
-        } while (response != 0);
+                    System.out.print("Debes seleccionar una opcion correcta \n");
+                }
+            } while (response != 0);
 
        
     }
@@ -39,29 +44,33 @@ public class uimenu {
     static void showPatienMenu() {
         int response = 0;
 
-        System.out.print("Menu Del paciente");
-        System.out.print("Seleccione una Opcion");
+        System.out.print("Menu Del paciente\n");
+        System.out.print("Seleccione una Opcion\n");
 
         do {
-            System.out.print("1. Historial medico");
-            System.out.print("2. Cita Medica");
-            System.out.print("0. salir del sistema");
+            System.out.print("MENU PACIENTE \n ");
+            System.out.print("1. Mes de cita medica\n");
+            System.out.print("2. Cita Medica\n");
+            System.out.print("0. Retornar\n");
 
             Scanner sc = new Scanner(System.in);
             response = Integer.valueOf(sc.nextLine());
 
             switch (response) {
                 case 1:
-                    System.out.print("Historia Medica");
+                    System.out.print("Mes de cita Medica\n");
+                    for (int i = 1; i<4; i++) 
+                    System.out.print(i + ". " + MONTH [i]+ "\n ");
                     break;
                 case 2:
-                    System.out.print("Cita Medica");
+                    System.out.print("Historial\n");
                     break;
                 case 0:
-                    System.out.print("Saliste del sistema");
+                    System.out.print("\n");
+                    showMenu();
                     break;
                 default:
-                    System.out.print("Debes seleccionar una opcion correcta");
+                    System.out.print("Debes seleccionar una opcion correcta\n");
 
             }
         } while (response != 0);
