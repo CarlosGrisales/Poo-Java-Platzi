195093e94bcdc089a3f88ebf32461b2d7a66fcd1
carlosstivengrisales <carlosstivengrisales@gmail.com>
2022-08-11 16:45:59 -0500

    Se agrega classe patient

---
+++ Medico.java
@@ -0,0 +1,19 @@
+public class Medico {
+    protected int id= 0;
+    protected String nameMedico;
+    protected String speciality;
+
+    public Medico(){
+        
+    }
+
+    public Medico(String nameMedico, String speciality) {
+        this.nameMedico = nameMedico;
+        this.speciality = speciality;
+    }
+
+
+
+
+    
+}
--- main.java
+++ main.java
@@ -1,21 +1,17 @@
-import java.util.jar.Attributes.Name;
 
 class main {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
+    Medico Mymedico = new Medico("Carlos Grisales", "Cardiologo");
+    System.out.print("\nEl nombre del medico es: " + Mymedico.nameMedico);
+    System.out.print("\nLa especialidad es: " + Mymedico.speciality);
 
+    Patient patient = new Patient("Dayanna", "Dayanna@mail.com");
+    patient.setWeight(54.6);
+    System.out.println(patient.getWeight());
 
-      uimenu.showMenu();
+    // uimenu.showMenu();
 
-
-      
-     
-
-   
-
-
-    }
-
-   
+  }
 
 }
